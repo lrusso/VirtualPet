b89bdb5690eab196c147a09abcbb48a2bf5b9e4d
Leonardo Russo <info@lrusso.com>
2021-04-24 14:19:45 -0300

    Add files via upload

--- VirtualPetGame.js
+++ VirtualPetGame.js
@@ -10,7 +10,7 @@ function isWebGLAvailable(){if(window.WebGLRenderingContext){for(var e=document.
 // isMobileDevice.js
 function isMobileDevice(){return!!(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))}
 
-var VirtualPet = {showDebug: false};
+var VirtualPet = {};
 
 VirtualPet.Preloader = function(){};
 
